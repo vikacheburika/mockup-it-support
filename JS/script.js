ac1cfecb9c652714292f3bab9058820021205979
Rytis <rytis.rupinskas@stud.techin.lt>
2026-05-25 08:24:29 +0300

    add technologies functionality

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const steps = document.querySelectorAll('.step');
+
+steps.forEach(step => {
+
+  step.addEventListener('mouseenter', () => {
+    removeActive();
+    step.classList.add('active');
+  });
+
+  step.addEventListener('click', () => {
+    removeActive();
+    step.classList.add('active');
+  });
+
+});
+
+function removeActive() {
+  steps.forEach(step => {
+    step.classList.remove('active');
+  });
+}

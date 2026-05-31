a17f48112af19eb848dd3b494dd1db155ae0e9b2
artioshf <vikacheburika@gmail.com>
2026-06-01 00:19:19 +0300

    Revert "Merge pull request #21 from vikacheburika/dev/ArtiomSiomin-clean"
    This reverts commit b2451a0a6de37fb99b2501b72a4d6f538f07fdb6, reversing
    changes made to 1fca214f1127f129266ea82720879f69869f77c7.

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,23 @@
 'use strict';
 
-let langBtn = document.getElementById('lang_btn');
-let languages = document.getElementById('langs');
-let engLang = document.getElementById("engl");
-let lithLang = document.getElementById("lthl");
-let langText = document.getElementById('en');
-let cnt = 0;
- languages.style.visibility = "hidden"
-langBtn.addEventListener('click', function(){
-   
-if (cnt % 2 === 0 ){
-    languages.style.visibility = "visible";
-    cnt++
+const steps = document.querySelectorAll('.step');
+
+steps.forEach(step => {
+
+  step.addEventListener('mouseenter', () => {
+    removeActive();
+    step.classList.add('active');
+  });
+
+  step.addEventListener('click', () => {
+    removeActive();
+    step.classList.add('active');
+  });
+
+});
+
+function removeActive() {
+  steps.forEach(step => {
+    step.classList.remove('active');
+  });
 }
-else if (cnt % 2 !== 0){
-    languages.style.visibility = "hidden"
-    cnt++
-}
-    
-});
-engLang.addEventListener("click", function(){
-    langText.textContent = "EN"
-    languages.style.visibility ="hidden"
-});
-lithLang.addEventListener("click", function(){
-    langText.textContent = "LT"
-    languages.style.visibility ="hidden"
-});
